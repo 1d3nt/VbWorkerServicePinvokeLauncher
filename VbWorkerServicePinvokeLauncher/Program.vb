8629d0f0760dee727522e063e55439118c9c08cb
Sam <44583516+1d3nt@users.noreply.github.com>
2024-10-13 22:05:09 +0100

    Program header updated: After roolback from master

--- VbWorkerServicePinvokeLauncher/Program.vb
+++ VbWorkerServicePinvokeLauncher/Program.vb
@@ -7,6 +7,8 @@
 ''' ORCID: <see href="https://orcid.org/0009-0007-1363-3308">https://orcid.org/0009-0007-1363-3308</see>
 ''' </author>
 ''' <date>31/08/2024</date>
+''' <version>1.0.0</version>
+''' <license>Creative Commons Attribution 4.0 International (CC BY 4.0) - See LICENSE.md for details</license>
 ''' <summary>
 ''' The entry point for the application.
 ''' </summary>
@@ -16,17 +18,21 @@
 ''' This project includes P/Invoke declarations and methods for interacting with Windows API functions.
 ''' Contributions and enhancements are welcomed to further extend the functionality and improve the integration.
 ''' 
-''' Just a hobby programmer that enjoys P/Invoke.
+''' Just a hobby programmer that enjoys P/Invoke and exploring complex interactions with system-level APIs.
+''' My mallory x
 ''' </remarks>
 Module Program
 
     ''' <summary>
-    ''' The main entry point for the application.
+    ''' Entry point for the console application. This application manages user interactions to configure and set up services. 
+    ''' It prompts users for input to determine whether to proceed with various setup tasks and performs actions based on their responses.
     ''' </summary>
-    ''' <param name="args">
-    ''' An array of <see cref="String"/> arguments passed to the application at startup.
-    ''' </param>
+    ''' <param name="args">Command-line arguments (not used in this implementation).</param>
     ''' <remarks>
+    ''' The <paramref name="args"/> parameter is included to adhere to the standard signature for a console application's
+    ''' <see cref="Main"/> method. While this parameter is not utilized in the current implementation, including it follows 
+    ''' best practices and ensures consistency with the conventional entry point signature.
+    ''' 
     ''' This method configures the application builder, sets up configuration files, registers the Windows service options,
     ''' and adds the <see cref="Worker"/> as a hosted service. After building the host, it starts the application.
     ''' 
@@ -34,6 +40,7 @@ Module Program
     ''' integration with Windows services. It leverages the .NET Generic Host to facilitate 
     ''' the management of background services and configuration.
     ''' </remarks>
+    <SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification:="Standard Main method parameter signature.")>
     Sub Main(args As String())
         Dim builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder(args)
         builder.Configuration.AddJsonFile("appsettings.json", optional:=True, reloadOnChange:=True)
